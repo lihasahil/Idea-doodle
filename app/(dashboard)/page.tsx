10d3a7c0ce19606a0a6a6062574b73f8a7eeb83e
lihasahil <sahilshrestha106@gmail.com>
2025-09-11 21:15:37 +0545

    feature: Project completion

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -14,7 +14,6 @@ interface DashboardPageProps {
 
 const DashboardPage = ({ searchParams }: DashboardPageProps) => {
   const { organization } = useOrganization();
-
   const query = use(searchParams);
 
   return (
